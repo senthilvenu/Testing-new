29fdd4b71df87b6e7f6f52f35bea8ea2b2116249
senthilvenu <35501598+senthilvenu@users.noreply.github.com>
2018-01-16 22:06:23 -0600

    Update testNGBatchFile.bat

--- Testing/testNGBatchFile.bat
+++ Testing/testNGBatchFile.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Users\siva\eclipse-workspace\Testing
+set projectLocation=C:\eclipse-committers-oxygen-1a-win32-x86_64\Selenium\Git hub\Testing
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
